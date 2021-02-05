6019d7bce5546cfb4bd45e3ba69c48f6849a0773
Paulo Salvatore <paulo@salvatore.tech>
2021-02-04 21:21:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ObjectId } = require('mongodb');
 (async () => {
     const url = 'mongodb+srv://admin:MVhyXlPJWaD2Cfbq@cluster0.7fu0x.mongodb.net/ocean_db?retryWrites=true&w=majority';
 
-    const dbName = ocean_db';
+    const dbName = 'ocean_db';
 
     console.info('Conectando ao banco de dados...');
 
